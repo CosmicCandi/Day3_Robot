65ab9c6adba7ea828d83e67166bd8abc92bcabef
CosmicCandi <kalea.wolff@gmail.com>
2017-02-01 21:17:31 -0500

    Finalizing code.  Made the output pretty. Code was already pretty. Epic mode COMPLETE! YASSSSS!

--- robots.rb
+++ robots.rb
@@ -26,9 +26,22 @@ end
 #Create a new robot instance
 robot = Robot.new("Kalea")
 #Call the say_hi and say_name methods and
+puts "This is the robot we just created: "
 puts robot.inspect
-robot.say_hi
-robot.say_name
+
+#Add a new Line
+puts "\n"
+
+puts "This is the robot saying Hi: "
+puts robot.say_hi
+
+#Add a new Line
+puts "\n"
+
+
+puts "This is the robot introducing itself: "
+puts robot.say_name
+puts "\n #\#\# \n "
 
 #Define a BendingUnit class
 # => A BendingUnit inherits from Robot
@@ -45,10 +58,17 @@ class BendingUnit < Robot
 end
 
 newbender = BendingUnit.new("Bender")
-#newbender.bend("A Shovel")
+puts "This Bending Unit is bending a statically assigned thing. Watch it... "
+newbender.bend("A Shovel")
+
+#Add a new Line
+puts "\n"
+
+puts "Now watch it.. "
 newbender.bend(ARGV[0])
 
-
+#Add a new Line
+puts "\n"
 
 #Define an ActorUnit class
 # => An ActorUnit inherits from Robot
@@ -63,23 +83,48 @@ class ActorUnit < Robot
  end
 end
 
+puts "We just created a new Actor Unit."
 newactor = ActorUnit.new("Mister Snuffles")
+
+puts "The new Actor Unit's name is Mister Snuffles"
 puts newactor.inspect
 newactor.say_name
+
+#Add a new Line
+puts "\n"
+
+puts "Mister Snuffles has been working hard on his new role! See him now as: "
 puts newactor.change_name = "Alabaster McBAMFington"
-newactor.inspect
+puts newactor.inspect
 newactor.say_name
+
+#Add a new Line
+puts "\n"
 
 ### Adventure Mode###
 #Take our student array from yesterday and (programmatically) create robots out of all of them and store them in an array.
 our_class = ["Ron", "David", "Nancy", "Kalea", "Laura", "Dave", "Demetra", "Phil",
 "Ben", "Kendrick", "Michael", "Miguel"]
+
+puts "Let's see what the our_class Array holds:"
+puts our_class.inspect
+
+#Add a new Line
+puts "\n"
+
+puts "Now we're going to iterate over that array and turn everybody into robots!
+Behold the Science Fiction-y goodness! Also note that they were randomly assigned
+a robot type! NEAT, right?!"
+
+#Add a new Line
+puts "\n"
+
 # => Do the previous using an Enumerable method other than each
 #for each name, turn it into a variable where variable = robot.new(variable) and then send it into an array.
 class_array = [Robot, BendingUnit, ActorUnit]
-robot_array = our_class.collect {|newrobot| class_array.sample.new(newrobot)}
+robot_array = our_class.collect {|newrobot|
+  ### Epic Mode ###
+  # Randomly assign the class of the robot on creation.
+  class_array.sample.new(newrobot)}
 
 puts robot_array.inspect
-
-### Epic Mode ###
-# Randomly assign the class of the robot on creation.
